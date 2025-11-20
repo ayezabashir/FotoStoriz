1bbc33faaf4a15fc2d8390f06a917c86539012ce
Ayeza Bashir <ayezabashir46@gmail.com>
2025-11-20 16:51:43 +0500

    defining routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Home from "./pages/Home"
+import Stories from "./pages/Stories"
+import Features from "./pages/Features"
+import Pricing from "./pages/Pricing"
 
 const App = () => {
   return (
     <>
-      <Home />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home/>} /> 
+          <Route path="/features" element={<Features/>} />
+          <Route path="/stories" element={<Stories/>} />
+          <Routes path="/pricing" element={<Pricing/>} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
