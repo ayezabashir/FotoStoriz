f915427ba3241c37af00e57663156335c190a5e5
Ayeza Bashir <ayezabashir46@gmail.com>
2025-11-21 22:32:50 +0500

    feature block

--- src/components/FeatureBlock.jsx
+++ src/components/FeatureBlock.jsx
@@ -1,9 +1,12 @@
 import { ContentSection } from './ContentSection'
 import img_desk from '/src/assets/img2_desktop.webp';
 import img_mb from '/src/assets/img2_mobile.webp';
+import img2_desk from '/src/assets/img3_desktop.webp';
+import img2_mb from '/src/assets/img3_mobile.webp';
 
 const FeatureBlock = () => {
   return (
+    <>
     <ContentSection
     title = "Timeless tales, every time"
     description = "Capture the moments that matter most. Every photo tells a story. From everyday memories to special milestones, we turn your life into beautiful stories."
@@ -12,6 +15,15 @@ const FeatureBlock = () => {
     reverse = {true}
     dark = {false}
     />
+    <ContentSection
+    title = "Made to fit every story"
+    description = "Whether you're a casual snapshooter or a professional photographer, our platform is designed to meet your needs. Enjoy seamless organization, easy sharing, and stunning displays tailored for all types of users."
+    img_mb = {img2_mb}
+    img_desk = {img2_desk}
+    reverse = {false}
+    dark = {true}
+    />
+    </>
   )
 }
 
