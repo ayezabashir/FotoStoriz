a65c645052d8904105bbf060608be12bddeb9c23
Ayeza Bashir <ayezabashir46@gmail.com>
2025-11-22 20:31:41 +0500

    header for mobile

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,9 +2,15 @@ import { useNavigate } from "react-router-dom"
 import menu from "/src/assets/menu.svg";
 import Button from "./Button";
 import HeaderMenuItems from "./HeaderMenuItems";
+import { useState } from "react";
 
 const Header = () => {
+  const [showMenuItems, setShowMenuItems]= useState(false);
   const nav = useNavigate();
+
+  const handleMenuItems = ()=>{
+    setShowMenuItems(!showMenuItems);
+  }
 
   return (
     <header className="max-w-[1200px] mx-auto">
@@ -12,10 +18,17 @@ const Header = () => {
         <div onClick={() => nav("/")}>
           <h1 className="text-3xl font-stretch-110% font-extrabold tracking-wide cursor-pointer">FotoStoriz</h1>
         </div>
-        <div className="block md:hidden">
-          <Button variant="secondary" >
+        <div className="block md:hidden relative">
+          <Button variant="secondary" onClick={handleMenuItems} >
             <img className="w-7" src={menu} alt="" />
           </Button>
+          {
+            showMenuItems && <div className="absolute right-0 bg-black/90 p-4 rounded-xl">
+            <div className="flex flex-col items-end">
+              <HeaderMenuItems />
+            </div>
+          </div>
+          }
         </div>
         <div className="hidden md:flex items-center justify-between gap-8">
           <HeaderMenuItems />
--- src/components/HeaderMenuItems.jsx
+++ src/components/HeaderMenuItems.jsx
@@ -5,12 +5,12 @@ const HeaderMenuItems = () => {
     const nav = useNavigate();
     return (
         <>
-            <ul className="flex items-center justify-between gap-8">
-                <li className="font-semibold tracking-widest text-black hover:underline underline-offset-2 transform transition-all delay-75 hover:-translate-y-1 cursor-pointer text-sm mx-1" onClick={() => nav("/stories")}>STORIES</li>
-                <li className="font-semibold tracking-widest text-black hover:underline underline-offset-2 transform transition-all delay-75 hover:-translate-y-1 cursor-pointer text-sm mx-1" onClick={() => nav("/features")}>FEATURES</li>
-                <li className="font-semibold tracking-widest text-black hover:underline underline-offset-2 transform transition-all delay-75 hover:-translate-y-1 cursor-pointer text-sm mx-1" onClick={() => nav("/pricing")}>PRICING</li>
+            <ul className="flex flex-col md:flex-row items-center justify-between gap-8">
+                <li className="font-semibold tracking-widest text-white md:text-black hover:underline underline-offset-2 transform transition-all delay-75 hover:-translate-y-1 cursor-pointer text-sm mx-1" onClick={() => nav("/stories")}>STORIES</li>
+                <li className="font-semibold tracking-widest text-white md:text-black hover:underline underline-offset-2 transform transition-all delay-75 hover:-translate-y-1 cursor-pointer text-sm mx-1" onClick={() => nav("/features")}>FEATURES</li>
+                <li className="font-semibold tracking-widest text-white md:text-black hover:underline underline-offset-2 transform transition-all delay-75 hover:-translate-y-1 cursor-pointer text-sm mx-1" onClick={() => nav("/pricing")}>PRICING</li>
             </ul>
-            <div>
+            <div className="mt-5 md:mt-0">
                 <Button variant="primary" size="md">
                     get an invite
                 </Button>
