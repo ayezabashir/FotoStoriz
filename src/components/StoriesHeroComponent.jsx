083ad595fd46d67c3fbf05f9fdca3d359a402b2f
Ayeza Bashir <ayezabashir46@gmail.com>
2025-11-25 17:28:50 +0500

    carousoul slider

--- src/components/StoriesHeroComponent.jsx
+++ src/components/StoriesHeroComponent.jsx
@@ -7,7 +7,7 @@ const StoriesHeroComponent = () => {
     let [currentId, setCurrentId] = useState(1);
     const handleScrollLeft = () => {
         setCurrentId((prev) => {
-            if (prev > 1) {
+            if (prev > 0) {
                 return prev - 1
             }
             return 2
@@ -18,16 +18,16 @@ const StoriesHeroComponent = () => {
             if (prev < 2) {
                 return prev + 1
             }
-            return 1
+            return 0
         })
     }
     console.log(currentId)
     return (
-        <div className='w-full md:h-[500px] lg:h-[600px] relative'>
-            <Button variant="secondary" size="md" className="absolute right-[50px] top-0 bottom-[50px] transform translate-[50%] z-10 h-[50px] w-[50px] cursor-pointer" onClick={handleScrollRight} >
+        <div className='w-full h-[500px] lg:h-[600px] relative'>
+            <Button variant="secondary" size="md" className="absolute right-0 top-0 bottom-[50px] transform translate-y-[180px]  md:translate-y-[250px] z-10 h-[50px] w-[50px] cursor-pointer" onClick={handleScrollRight} >
                 <img className="w-5 h-5" src={arrow} alt="" />
             </Button>
-            <Button variant="secondary" size="md" className="absolute left-[50px] top-0 bottom-[50px] transform translate-[50%] z-10 h-[50px] w-[50px] cursor-pointer" onClick={handleScrollLeft} >
+            <Button variant="secondary" size="md" className="absolute left-0 top-0 bottom-[50px] transform translate-y-[180px] md:translate-y-[250px] z-10 h-[50px] w-[50px] cursor-pointer" onClick={handleScrollLeft} >
                 <img className="w-5 h-5 transform rotate-180" src={arrow} alt="" />
             </Button>
             {
@@ -39,11 +39,11 @@ const StoriesHeroComponent = () => {
                         </picture>
                     </div>
                     <div className="absolute inset-0 bg-linear-to-t from-black/60 to-transparent"></div>
-                    <div className="absolute top-0 bottom-0 left-0 z-[10px] p-2 flex flex-col justify-center items-start">
-                        <p className="uppercase text-white text-base">Last month featured story</p>
-                        <h2 className="uppercase text-white text-2xl">{featureStory[currentId].title}</h2>
-                        <h3 className="text-white">{featureStory[currentId].date} by {featureStory[currentId].author.name}</h3>
-                        <p className="text-white">
+                    <div className="absolute top-0 bottom-0 left-0 z-[10px] md:w-[600px] p-10 md:p-16 flex flex-col justify-center items-start">
+                        <p className="uppercase text-gray-200 text-sm tracking-wider">Last month featured story</p>
+                        <h2 className="uppercase text-white my-3 tracking-wide text-3xl md:text-5xl">{featureStory[currentId].title}</h2>
+                        <h3 className="text-base text-gray-200">{featureStory[currentId].date} by <b className="text-white">{featureStory[currentId].author.name}</b></h3>
+                        <p className="text-gray-200 mt-7 text-sm tracking-wide leading-6">
                             {featureStory[currentId].content}
                         </p>
                     </div>
