23d4820f9f4b9862bd97fa35ce0b68579f289eb6
Ayeza Bashir <ayezabashir46@gmail.com>
2025-11-26 15:46:02 +0500

    feature card

--- src/components/IconCards.jsx
+++ src/components/IconCards.jsx
@@ -2,8 +2,8 @@ const IconCards = ({image, heading, paragraph}) => {
   return (
     <div className='flex flex-col justify-center items-center gap-3 p-2 group'>
         <img className='w-24 mb-5 transition-transform delay-75 group-hover:scale-105' src={image} alt="feature icon" />
-        <h3 className='font-bold font-xl'>{heading}</h3>
-        <p className='text-base leading-6 text-center'>{paragraph}</p>
+        <h3 className='font-bold font-2xl'>{heading}</h3>
+        <p className='text-sm font-regular leading-6 text-center'>{paragraph}</p>
     </div>
   )
 }
