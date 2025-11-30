1a2c7c410140c214055be960fe831546bd5c1a6f
Ayeza Bashir <ayezabashir46@gmail.com>
2025-11-30 18:05:56 +0500

    configured navigation

--- src/components/StoriesHeroComponent.jsx
+++ src/components/StoriesHeroComponent.jsx
@@ -1,10 +1,44 @@
 import { useState } from "react";
-import { featureStory } from "../utils/stories"
 import Button from "./Button"
 import arrow from "/src/assets/right-arrow-white.svg";
+import { useNavigate } from "react-router-dom";
 
-const StoriesHeroComponent = () => {
+const StoriesHeroComponent = (
+    {
+        key,
+        date,
+        img_desk,
+        img_mob,
+        title,
+        author_name,
+        author_email,
+        author_address,
+        author_img,
+        categories,
+        content,
+        slug,
+    }
+) => {
     let [currentId, setCurrentId] = useState(1);
+    const nav = useNavigate();
+    const handleNav = () => {
+        nav(`/story/${slug}`, {
+            state: {
+                key,
+                date,
+                img_desk,
+                img_mob,
+                title,
+                author_name,
+                author_email,
+                author_address,
+                author_img,
+                categories,
+                content,
+                slug,
+            }
+        })
+    }
     const handleScrollLeft = () => {
         setCurrentId((prev) => {
             if (prev > 0) {
@@ -21,7 +55,7 @@ const StoriesHeroComponent = () => {
             return 0
         })
     }
-    console.log(currentId)
+
     return (
         <div className='w-full h-[500px] lg:h-[600px] relative'>
             <Button variant="secondary" size="md" className="absolute right-0 top-0 bottom-[50px] transform translate-y-[180px]  md:translate-y-[250px] z-10 h-[50px] w-[50px] cursor-pointer" onClick={handleScrollRight} >
@@ -32,20 +66,20 @@ const StoriesHeroComponent = () => {
             </Button>
             {
                 <>
-                    <div key={featureStory[currentId].id} className='h-full w-full'>
+                    <div key={currentId} className='h-full w-full'>
                         <picture>
-                            <source media='(min-width:768px)' srcSet={featureStory[currentId].img_mb} />
-                            <img src={featureStory[currentId].img_desk} className="w-full h-full object-cover object-center" alt="full moon background image" />
+                            <source media='(min-width:768px)' srcSet={img_mob} />
+                            <img src={img_desk} className="w-full h-full object-cover object-center" alt="full moon background image" />
                         </picture>
                     </div>
                     <div className="absolute inset-0 bg-linear-to-t from-black/60 to-transparent"></div>
                     <div className="absolute top-0 bottom-0 left-0 z-[10px] md:w-[600px] p-10 md:p-16 flex flex-col justify-center items-start">
                         <p className="uppercase text-gray-200 text-sm tracking-wider">Last month featured story</p>
-                        <h2 className="uppercase text-white my-3 tracking-wide text-3xl md:text-5xl">{featureStory[currentId].title}</h2>
-                        <h3 className="text-base text-gray-200">{featureStory[currentId].date} by <b className="text-white">{featureStory[currentId].author.name}</b></h3>
-                        <p className="text-gray-200 mt-7 text-sm tracking-wide leading-6">
-                            {featureStory[currentId].content}
-                        </p>
+                        <h2 className="uppercase text-white my-3 tracking-wide text-3xl md:text-5xl">{title}</h2>
+                        <h3 className="text-base text-gray-200">{date} by <b className="text-white">{author_name}</b></h3>
+                        <Button className="text-white text-start mt-4" variant="secondary" size="lg" onClick={handleNav} >
+                            Read Story
+                        </Button>
                     </div>
                 </>
             }
--- src/pages/Stories.jsx
+++ src/pages/Stories.jsx
@@ -1,16 +1,15 @@
 import React from 'react'
 import { stories } from '../utils/stories'
+import { featureStory } from '../utils/stories'
 import StoryCard from '../components/StoryCard'
 import StoriesHeroComponent from '../components/StoriesHeroComponent'
 
 const Stories = () => {
   return (
     <>
-      <StoriesHeroComponent />
-      <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4'>
-        {
-        stories.map((story) =>
-          <StoryCard
+      {
+        featureStory.map((story =>
+          <StoriesHeroComponent
             key={story.id}
             date={story.date}
             img_desk={story.img_desk}
@@ -24,8 +23,27 @@ const Stories = () => {
             content={story.content}
             slug={story.slug}
           />
-        )
+        ))
       }
+      <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4'>
+        {
+          stories.map((story) =>
+            <StoryCard
+              key={story.id}
+              date={story.date}
+              img_desk={story.img_desk}
+              img_mob={story.img_mb}
+              title={story.title}
+              author_name={story.author.name}
+              author_email={story.author.email}
+              author_address={story.author.address}
+              author_img={story.author.img}
+              categories={story.categories}
+              content={story.content}
+              slug={story.slug}
+            />
+          )
+        }
       </div>
     </>
   )
