b8521aff1ba3afc3556a4990c0b6bfe980ea78d7
Ayeza Bashir <ayezabashir46@gmail.com>
2025-12-01 22:28:40 +0500

    pricing plans

--- src/components/PricingPlanComponent.jsx
+++ src/components/PricingPlanComponent.jsx
@@ -1,8 +1,13 @@
-import React from 'react'
-
-const PricingPlanComponent = () => {
+const PricingPlanComponent = ({plan_name, plan_price, plan_benefits, plan_btn}) => {
   return (
-    <div>PricingPlanComponent</div>
+    <div className='max-h-[500px] w-[300px] border-2 rounded-lg'>
+      <div className="bg-black">
+        <h1 className="text-center font-bold text-lg text-white px-3 py-6">{plan_name}</h1>
+      </div>
+      <div>
+        
+      </div>
+    </div>
   )
 }
 
--- src/components/PricingPlans.jsx
+++ src/components/PricingPlans.jsx
@@ -5,36 +5,42 @@ const plans = [
     plan_name: "Free Plan",
     price: "Free",
     benefits: ["20 uploads", "5 stories", "Basic templates "],
+    bg_purple: false,
     btn_text: "Start Free"
   },
   {
     plan_name: "Pro Plan",
     price: "$300",
     benefits: ["Unlimited uploads", "Unlimited stories", "Custom templates"],
+    bg_purple: true,
     btn_text: "Upgrade Now"
   },
   {
     plan_name: "Creator Plan",
     price: "$800",
     benefits: ["Everything in Pro", "Collaboration", "Export tools"],
+    bg_purple: false,
     btn_text: "Go Creator"
   }
 ]
 
 const PricingPlans = () => {
   return (
-    <div className='mx-7 my-4'>
-      {
-        plans.map((plan, index) => (
-          <PricingPlanComponent
-            key={index}
-            plan_name={plan.plan_name}
-            plan_price={plan.price}
-            plan_benefits={plan.benefits}
-            plan_btn={plan.btn_text}  
-          />
-        ))
-      }
+    <div className='mx-7 my-36'>
+      <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 items-center'>
+        {
+          plans.map((plan, index) => (
+            <PricingPlanComponent
+              key={index}
+              plan_name={plan.plan_name}
+              plan_price={plan.price}
+              plan_benefits={plan.benefits}
+              bg_purple={plan.bg_purple}
+              plan_btn={plan.btn_text}
+            />
+          ))
+        }
+      </div>
     </div>
   )
 }
