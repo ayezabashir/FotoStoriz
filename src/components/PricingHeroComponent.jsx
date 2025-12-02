3bed41091aced8b835b31c3a41fffdaf062c68cf
Ayeza Bashir <ayezabashir46@gmail.com>
2025-12-02 19:09:24 +0500

    pricing plan update

--- src/components/PricingHeroComponent.jsx
+++ src/components/PricingHeroComponent.jsx
@@ -2,20 +2,20 @@ const PricingHeroComponent = () => {
   return (
     <div className="relative grid grid-cols-1 md:grid-cols-2 items-center h-[300px] mx-7 my-4">
       <div className="w-full hidden md:block">
-        <div className="absolute left-0 top-0 w-72 h-72 lg:w-96 lg:h-96 bg-linear-to-br from-purple-500 to-blue-500 rounded-full blur-3xl opacity-30"></div>
+        <div className="absolute left-0 top-10 w-72 h-72 lg:w-80 lg:h-80 bg-linear-to-br from-gray-200 to-black rounded-full blur-3xl opacity-30"></div>
       </div>
       <div className="max-w-[400px pl-5]">
-        <h1 className='text-2xl my-5 font-semibold tracking-wide capitalize '>
+        <h1 className='text-3xl my-5 font-bold tracking-wide capitalize '>
           Simple pricing for storytellers.
         </h1>
-        <p className="text-base mb-2 text-purple-500">
+        <p className="text-base mb-2 text-black">
           No hidden fees, no limits.  
         </p>
         <p className="italic text-sm">Choose a plan that works best for you and your audience.</p>
         <ul className="mt-5">
-          <li className="text-blue-500 py-1 text-base">✓  Unlimited uploads</li>
-          <li className="text-blue-500 py-1 text-base">✓  Cancel anytime</li>
-          <li className="text-blue-500 py-1 text-base">✓  No ads</li>
+          <li className="text-black py-1 text-base">✓  Unlimited uploads</li>
+          <li className="text-black py-1 text-base">✓  Cancel anytime</li>
+          <li className="text-black py-1 text-base">✓  No ads</li>
         </ul>
       </div>
       
--- src/components/PricingPlanComponent.jsx
+++ src/components/PricingPlanComponent.jsx
@@ -6,14 +6,14 @@ const PricingPlanComponent = ({plan_name, plan_desc, plan_price,bg_purple, plan_
       <div className="bg-black">
         <h1 className="text-center font-bold text-lg text-white px-3 py-6">{plan_name}</h1>
       </div>
-      <div className={`${bg_purple===true ? "bg-purple-400 text-white h-full" : "bg-white text-black"} px-4 py-8`}>
-        <p className={`${bg_purple===true ? " text-white" : "text-black"} text-center capitalize text-sm font-semibold tracking-wide`}>{plan_desc}</p>
-        <h2 className="text-6xl md:text-5xl lg:text-6xl text-blue-500 font-black text-center my-5">{plan_price}</h2>
+      <div className={`${bg_purple===true ? "bg-[#999999] h-full" : "bg-white "} text-black px-4 py-8`}>
+        <p className="text-center capitalize text-sm font-semibold tracking-wide text-black">{plan_desc}</p>
+        <h2 className="text-6xl md:text-5xl lg:text-6xl text-black font-black text-center my-5">{plan_price}</h2>
         <h3 className="text-lg text-center font-semibold my-5">What Comes With It?</h3>
         <ul className="px-5">
           {
             plan_benefits.map((item, index)=>(
-              <li key={index} className={`${bg_purple===true ? " text-white" : "text-black"} py-1 list-item list-disc`}>{item}</li>
+              <li key={index} className="text-black py-1 list-item list-disc ">{item}</li>
             ))
           }
         </ul>
