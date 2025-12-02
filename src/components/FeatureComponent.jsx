1f452f66e34be1743cee3d840d140e6208d2ffdc
Ayeza Bashir <ayezabashir46@gmail.com>
2025-12-02 21:49:24 +0500

    feature page updated

--- src/components/FeatureComponent.jsx
+++ src/components/FeatureComponent.jsx
@@ -1,17 +1,23 @@
+import Button from "./Button"
+
 const FeatureComponent = ({ heading, icon, desc }) => {
   return (
-    <div className=' h-[500px] w-1/4'>
-      <div className='h-[40%] bg-white text-black text-center p-5'>
+    <div className=' h-[380px] '>
+      <div className='h-[50%] bg-white text-black text-center p-5'>
         <h2 className="font-semibold text-lg">
           {heading}
         </h2>
         <picture className="flex justify-center">
-          <img className="w-20 h-20 my-3" src={icon} alt="" />
+          <img className="w-16 h-16 my-3" src={icon} alt="" />
         </picture>
       </div>
-      <div className='h-[60%] bg-black '>
+      <div className='h-[50%] bg-black py-2 '>
         <p className='p-4 text-center text-white'>
-          {desc} </p>
+          {desc} 
+        </p>
+        <Button variant="secondary" size="md" className="mx-auto border border-white text-white p-2 hover:scale-105 transition-all">
+          Learn More
+        </Button>
       </div>
     </div>
   )
--- src/components/FeatureHeroComponent.jsx
+++ src/components/FeatureHeroComponent.jsx
@@ -8,22 +8,22 @@ const features = [
   {
     heading: "Story-Driven Photo Layouts",
     icon: layout,
-    desc : "FotoStoriz transforms your images into immersive, narrative-based layouts. Each story section is crafted to highlight emotions & moments"
+    desc : "FotoStoriz transforms your images into immersive, narrative-based layouts."
   },
   {
     heading: "Dynamic Hero Story Showcase",
     icon: sparkle,
-    desc: "Your best story or featured collection automatically appears in a premium hero section with large imagery, date, and context. "
+    desc: "Your best story or featured collection automatically appears in a premium section."
   },
   {
     heading: "Interactive Story Featured Cards",
     icon: cards,
-    desc: "Every story is displayed through elegant, hover-responsive cards that reveal more details as users interact. "
+    desc: "Every story is displayed through elegant, hover-responsive cards that reveal more"
   },
   {
     heading: "Fully Responsive & Optimized",
     icon: devices,
-    desc: "From large desktop screens to mobile phones, FotoStoriz adapts seamlessly. Images resize intelligently, text scales for readability"
+    desc: "From large desktop screens to mobile phones, FotoStoriz adapts seamlessly."
   }
 ]
 
@@ -33,7 +33,7 @@ const FeatureHeroComponent = () => {
         <div className="max-w-[1200px] py-10 mx-auto">
             <div className="mx-7 text-center">
               <h1 className="text-3xl font-bold text-white">Checkout All the Features</h1>
-              <div className="flex justify-between gap-8">
+              <div className="mt-[34px] grid grid-cols-2 md:grid-cols-4 w-full gap-8">
                 {
                   features.map((feature, index)=>(
                     <FeatureComponent key={index} heading={feature.heading} icon={feature.icon} desc={feature.desc} />
