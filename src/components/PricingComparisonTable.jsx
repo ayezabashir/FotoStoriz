3e961469fe324edbcacd056e2a31bd5f7f0107f0
Ayeza Bashir <ayezabashir46@gmail.com>
2025-12-02 18:30:00 +0500

    comparison table component

--- src/components/PricingComparisonTable.jsx
+++ src/components/PricingComparisonTable.jsx
@@ -2,7 +2,48 @@ import React from 'react'
 
 const PricingComparisonTable = () => {
   return (
-    <div>PricingComparisonTable</div>
+    <div className='mx-4 flex flex-col items-center justify-center'>
+      <div className='max-w-[420px] text-center text-purple-400 mb-7'>
+        <h2 className='font-black text-3xl mb-7'>Feature Comparison Table</h2>
+        <p>Compare Features to find the perfect plan for your needs</p>
+      </div>
+      <table className='w-full bg-purple-50 rounded-lg '>
+        <thead>
+          <tr className='border-b border-gray-300'>
+            <th className='p-4 text-start'>Features</th>
+            <th className='p-4'>Free Plan</th>
+            <th className='p-4'>Pro Plan</th>
+            <th className='p-4'>Creator Plan</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr className='border-b border-gray-300'>
+            <td className='p-4'>Unlimited photos</td>
+            <td className='p-4 text-center '>✖</td>
+            <td className='p-4 text-center text-purple-400'>✔ </td>
+            <td className='p-4 text-center text-purple-400'>✔ </td>
+          </tr>
+          <tr className='border-b border-gray-300'>
+            <td className='p-4'>Analytics </td>
+            <td className='p-4 text-center'>✖</td>
+            <td className='p-4 text-center text-purple-400'>✔</td>
+            <td className='p-4 text-center text-purple-400'>✔</td>
+          </tr>
+          <tr className='border-b border-gray-300'>
+            <td className='p-4'>Custom templates </td>
+            <td className='p-4 text-center'>✖</td>
+            <td className='p-4 text-center text-purple-400'>✔</td>
+            <td className='p-4 text-center text-purple-400'>✔</td>
+          </tr>
+          <tr>
+            <td className='p-4'>Collaboration </td>
+            <td className='p-4 text-center'>✖</td>
+            <td className='p-4 text-center'>✖</td>
+            <td className='p-4 text-center text-purple-400'>✔</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   )
 }
 
