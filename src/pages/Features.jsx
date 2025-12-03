dd7c4f1d0f0fa979172091cc2ea64f52417c8732
Ayeza Bashir <ayezabashir46@gmail.com>
2025-12-03 22:52:03 +0500

    features update

--- src/pages/Features.jsx
+++ src/pages/Features.jsx
@@ -1,11 +1,17 @@
 import FeatureHeroComponent from '../components/FeatureHeroComponent'
 import FeatureIcons from '../components/FeatureIcons'
+import PricingComparisonTable from '../components/PricingComparisonTable'
 
 const Features = () => {
   return (
     <div>
       <FeatureHeroComponent />
-      <FeatureIcons />
+      <div className='max-w-[900px] mx-auto'>
+      <PricingComparisonTable />
+      </div>
+      <div className='max-w-[900px] mx-auto my-10 border-2 rounded-2xl border-black'>
+        <FeatureIcons />
+      </div>
     </div>
   )
 }
--- src/pages/Pricing.jsx
+++ src/pages/Pricing.jsx
@@ -1,7 +1,5 @@
-import React from 'react'
 import PricingHeroComponent from '../components/PricingHeroComponent'
 import PricingPlans from '../components/PricingPlans'
-import PricingComparisonTable from '../components/PricingComparisonTable'
 import PricingFaqs from '../components/PricingFaqs'
 
 const Pricing = () => {
@@ -9,7 +7,6 @@ const Pricing = () => {
     <div className='max-w-[1200px] mx-auto'>
       <PricingHeroComponent />
       <PricingPlans />
-      <PricingComparisonTable />
       <PricingFaqs />
     </div>
   )
