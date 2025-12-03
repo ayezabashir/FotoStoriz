aa6eca0dc078f03b9796d572280baa3f28682ab5
Ayeza Bashir <ayezabashir46@gmail.com>
2025-12-03 22:54:29 +0500

    feature page updated

--- src/pages/Features.jsx
+++ src/pages/Features.jsx
@@ -9,9 +9,7 @@ const Features = () => {
       <div className='max-w-[900px] mx-auto'>
       <PricingComparisonTable />
       </div>
-      <div className='max-w-[900px] mx-auto my-10 border-2 rounded-2xl border-black'>
-        <FeatureIcons />
-      </div>
+     <FeatureIcons />
     </div>
   )
 }
